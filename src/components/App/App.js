9461fb04e3263ff23448b82ce42c184f732b47be
Patryk Hensel <patrykhensa@gmail.com>
2024-06-19 22:48:56 +0200

    Fix background source

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react'
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
-import './App.css';
+// import './App.css';
 
 
 const App = () => {
@@ -36,7 +36,8 @@ const App = () => {
 
   return (
     <div>
-      <div className="App">
+      <h1 className="text-4xl font-bold text-white bg-gray-800 p-4">Prep2Play</h1>
+      <div className="text-center bg-[url('/Designer.jpeg')] h-full px-[17%] pt-0 pb-[10%] bg-cover bg-center bg-no-repeat font-sans font-medium text-white">
         <SearchBar onSearch={search} />
         <SearchResults searchResults={searchResults} onAdd={addTrack} />
         <Playlist playlistTracks={playlistTracks} onRemove={removeTrack} />
